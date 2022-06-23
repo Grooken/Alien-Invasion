165981b4ab6ac75ed94b88b61fb16675c33e1cf8
Anton Zherebetsky <anton.zherebeckii@gmail.com>
2022-06-23 15:29:07 +0300

    Скрываем указатель мыши после нажатия на кнопку "Play".

--- game_functions.py
+++ game_functions.py
@@ -57,6 +57,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
 
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Указатель мыши скрывается.
+        pygame.mouse.set_visible(False)
         # Сброс игровой статистики.
         stats.reset_stats()
         stats.game_active = True
@@ -208,6 +210,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
