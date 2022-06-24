61f26a218e2d4eefcaa5c909bdce568862a6ef70
Anton Zherebetsky <anton.zherebeckii@gmail.com>
2022-06-24 13:19:15 +0300

    Визуальные доработки.

--- settings.py
+++ settings.py
@@ -8,15 +8,15 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         # Назначение цвета фона (RGB)
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (200, 200, 200)
 
         # Настройки корабля
         self.ship_limit = 3
         # Настройки пришельцев
         self.fleet_drop_speed = 10
         # Параметры пули
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_width = 5
+        self.bullet_height = 20
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
 
